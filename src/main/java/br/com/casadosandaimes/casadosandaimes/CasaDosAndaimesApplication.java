906298b5d4c96df952643b194c2cd4ce6d4534b8
Matheus Fagundes <matewsfagundes@gmail.com>
2023-04-01 22:38:42 -0300

    inica o projeto

---
+++ src/main/java/br/com/casadosandaimes/casadosandaimes/CasaDosAndaimesApplication.java
@@ -0,0 +1,13 @@
+package br.com.casadosandaimes.casadosandaimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDosAndaimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDosAndaimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casadosandaimes/casadosandaimes/CasaDosAndaimesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casadosandaimes.casadosandaimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDosAndaimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
