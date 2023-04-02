a22eb8c909cb7634bae7bc0f3b6156ce63e98d89
Matheus Fagundes <matewsfagundes@gmail.com>
2023-04-01 23:29:58 -0300

    implementa api de Teste

--- src/main/java/br/com/casadosandaimes/casadosandaimes/CasaDosAndaimesApplication.java
+++ src/main/java/br/com/casadosandaimes/casadosandaimes/CasaDosAndaimesApplication.java
@@ -2,12 +2,22 @@ package br.com.casadosandaimes.casadosandaimes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CasaDosAndaimesApplication {
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "Casa dos andaimes – API Home";
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasaDosAndaimesApplication.class, args);
 	}
-
 }
