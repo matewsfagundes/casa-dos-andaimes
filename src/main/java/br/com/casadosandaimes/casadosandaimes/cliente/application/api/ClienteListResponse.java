319cd39c0a52850f8c5c5f446c0081eeafc3ba43
Matheus Fagundes <matewsfagundes@gmail.com>
2023-04-02 21:06:57 -0300

    implementa fluxo api, teste ok

---
+++ src/main/java/br/com/casadosandaimes/casadosandaimes/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,12 @@
+package br.com.casadosandaimes.casadosandaimes.cliente.application.api;
+
+import java.util.UUID;
+
+public class ClienteListResponse {
+
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String email;
+	private String celular;
+}
